d9e6b88aa2b42dce73460e13a5c6e542d53955b3
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-01-24 12:09:49 -0800

    Initial Commit

---
+++ kehillah.today-swiftuitestyes/kehillah_today_swiftuitestyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  kehillah_today_swiftuitestyesApp.swift
+//  kehillah.today-swiftuitestyes
+//
+//  Created by Ovlic B on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct kehillah_today_swiftuitestyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
